8561760a1876548f4ba34e6f50e76ab0902a83e8
Elliot Nelson <nelson.work@gmail.com>
2020-05-01 14:18:39 -0400

    :art: Update screenshot

--- scripts/update-screenshot.sh
+++ scripts/update-screenshot.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#
-# This utility script is designed to quickly and easily update the style screenshot.
-# It's designed to run specifically on my (the author's) machine, so your mileage
-# may vary, no guarantees, etc.
 
-DIR=`pwd`
+# Confirm
+read -p "This script will resize your current terminal window. Press ENTER to continue."
 
-osascript <<END
-tell application "Terminal"
-    do script "
-        prompt_pure_set_title() {}
-        cd \"$PWD\"
-        scripts/update-screenshot-term.sh
-        sleep 1
-        exit
-    "
-end tell
-END
+# Config
+ROWS=30
+COLUMNS=110
+
+# Prep terminal window title and size
+printf "\033]0;%s\007" ""
+printf "\033[8;${ROWS};${COLUMNS}t"
+
+sleep 1
+clear
+
+# Prep screenshot
+echo "$ pizza cancel"
+node sample-cli.js cancel
+printf "$ "
+sleep 1
+screencapture -w screenshot.png
+mogrify -resize 50% screenshot.png
